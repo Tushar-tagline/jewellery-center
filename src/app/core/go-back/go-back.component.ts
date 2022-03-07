4a561dc396ac5d5d134f76755d8cad37cb04ea3b
tushar-tagline <tushar.tagline@gmail.com>
2022-03-07 11:15:55 +0530

    create goback button usign input decoreter

--- src/app/core/go-back/go-back.component.ts
+++ src/app/core/go-back/go-back.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'jewellery-center-go-back',
@@ -7,9 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GoBackComponent implements OnInit {
 
+  @Input() goBack?= 'Back to Page';
+
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
 
 }
