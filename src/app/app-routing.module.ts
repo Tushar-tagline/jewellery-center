e3c0f931e7757b868838dd696b0aca30cb85c364
tushar-tagline <tushar.tagline@gmail.com>
2022-03-07 11:17:05 +0530

    use to lazycoding

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'user', loadChildren: () => import('./ features/user/user.module').then(m => m.UserModule)
+  },
+  {
+    path: 'customer', loadChildren: () => import('./ features/customer/customer.module').then(m => m.CustomerModule)
+  },
+  {
+    path: '**',
+    redirectTo: 'user'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
