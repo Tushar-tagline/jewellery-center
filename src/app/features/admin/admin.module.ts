a3f1cc3e832143dbffdc644788ca261305e2c95c
tushar-tagline <tushar.tagline@gmail.com>
2022-03-08 17:53:44 +0530

    create viewproduct componect with routing

--- src/app/features/admin/admin.module.ts
+++ src/app/features/admin/admin.module.ts
@@ -5,16 +5,19 @@ import { AdminRoutingModule } from './admin-routing.module';
 
 import { CoreModule } from 'src/app/core/core.module';
 import { AdminHeaderComponent } from 'src/app/core/admin-header/admin-header.component';
+import { AddproductComponent } from './addproduct/addproduct.component';
+import { ViewproductComponent } from './viewproduct/viewproduct.component';
 
 
 @NgModule({
   declarations: [
-   
+    AddproductComponent,
+    ViewproductComponent
   ],
   imports: [
     CommonModule,
     AdminRoutingModule,
-  
- ]
+
+  ]
 })
 export class AdminModule { }
---
+++ src/app/features/admin/viewproduct/viewproduct.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jewellery-center-viewproduct',
+  templateUrl: './viewproduct.component.html',
+  styleUrls: ['./viewproduct.component.scss']
+})
+export class ViewproductComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
