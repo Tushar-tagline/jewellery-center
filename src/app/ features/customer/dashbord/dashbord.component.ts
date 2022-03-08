54af39b3fe07c9e2248291916e1b2dbd0988b0e8
tushar-tagline <tushar.tagline@gmail.com>
2022-03-08 10:03:07 +0530

    create dashborad componect and routing

---
+++ src/app/ features/customer/dashbord/dashbord.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jewellery-center-dashbord',
+  templateUrl: './dashbord.component.html',
+  styleUrls: ['./dashbord.component.scss']
+})
+export class DashbordComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
   },
   {
     path: '**',
-    redirectTo: 'user'
+    redirectTo: '/customer/home'
   }
 ];
 
