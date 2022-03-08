1118d727724a4b9d41537a63949963ed2bc57023
tushar-tagline <tushar.tagline@gmail.com>
2022-03-08 17:52:52 +0530

    create addproduct componect with routing

---
+++ src/app/features/admin/addproduct/addproduct.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jewellery-center-addproduct',
+  templateUrl: './addproduct.component.html',
+  styleUrls: ['./addproduct.component.scss']
+})
+export class AddproductComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/features/admin/admin-routing.module.ts
+++ src/app/features/admin/admin-routing.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminDashboardComponent } from '../admin-dashboard/admin-dashboard.component';
+import { AddproductComponent } from './addproduct/addproduct.component';
+import { ViewproductComponent } from './viewproduct/viewproduct.component';
 
 
 const routes: Routes = [
   {
-    path:'home',
-    component:AdminDashboardComponent
+    path: 'home',
+    component: AdminDashboardComponent
   },
   {
-    path:'**',
-    redirectTo:'admin'
+    path: 'addproduct',
+    component: AddproductComponent
+  },
+  {
+    path: 'viewproduct',
+    component: ViewproductComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'admin'
   }
 ];
 
