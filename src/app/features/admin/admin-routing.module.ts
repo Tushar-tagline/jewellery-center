37c4866ad4f544e7bccbf80fb30355b6719c9956
tushar-tagline <tushar.tagline@gmail.com>
2022-03-09 17:41:56 +0530

    use children routing in admin.module.ts

--- src/app/features/admin/admin-routing.module.ts
+++ src/app/features/admin/admin-routing.module.ts
@@ -3,24 +3,34 @@ import { RouterModule, Routes } from '@angular/router';
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 import { AddproductComponent } from './addproduct/addproduct.component';
 import { ViewproductComponent } from './viewproduct/viewproduct.component';
-
+import { EditProductComponent } from './edit-product/edit-product.component';
 
 const routes: Routes = [
   {
-    path: 'home',
-    component: AdminDashboardComponent
-  },
-  {
-    path: 'addproduct',
-    component: AddproductComponent
-  },
-  {
-    path: 'viewproduct',
-    component: ViewproductComponent
+    path: '',
+    component: AdminDashboardComponent,
+    children: [
+      {
+        path: 'add-product',
+        component: AddproductComponent
+      },
+      {
+        path: 'view-product',
+        component: ViewproductComponent
+      },
+      {
+        path: 'edit-product',
+        component: EditProductComponent
+      },
+      {
+        path: '**',
+        redirectTo: 'view-product'
+      }
+    ]
   },
   {
     path: '**',
-    redirectTo: 'admin'
+    redirectTo: ''
   }
 ];
 
