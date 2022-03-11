90c33d865b519e35e802ce0f38c584993a3cf6ac
tushar-tagline <tushar.tagline@gmail.com>
2022-03-11 12:31:47 +0530

    create customer.service

---
+++ src/app/shared/service/customer.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CustomerService {
+
+  public selectedCategory$ : any = new BehaviorSubject('ring');
+
+  constructor() { }
+}
