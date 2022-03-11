0d775386aa442499a846ed63cd2da738b101d3af
tushar-tagline <tushar.tagline@gmail.com>
2022-03-11 12:28:21 +0530

    create sub-header.component

---
+++ src/app/features/customer/sub-header/sub-header.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CustomerService } from 'src/app/shared/service/customer.service';
+
+@Component({
+  selector: 'jewellery-center-sub-header',
+  templateUrl: './sub-header.component.html',
+  styleUrls: ['./sub-header.component.scss']
+})
+export class SubHeaderComponent implements OnInit {
+
+  constructor(private router: Router, private customerService: CustomerService) { }
+
+  ngOnInit(): void {
+  }
+
+  setSelectedCategory(category: any, redirectUrl: any) {
+    this.customerService.selectedCategory$.next(category);
+    this.router.navigate([redirectUrl]);
+  }
+}
